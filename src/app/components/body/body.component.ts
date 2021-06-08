a31ed16c1679ae3f4e2e85fd967e55034123257d
Facundo Argüello <Facuatolosa@gmail.com>
2021-06-08 14:48:23 -0300

    Primer Proyecto Frontend

---
+++ src/app/components/body/body.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-body',
+    templateUrl: './body.component.html'
+})
+
+export class BodyComponent {
+    title = 'DAW 2021 - UTN FRVM';
+    developer = 'Argüello Tolosa, Facundo'
+}
